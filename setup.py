df846f30b69dae51a3dc5040869f79cb30f22898
InstallScript <installscript@izbicki.me>
2017-02-07 00:03:06 -0800

    initial

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='jupyter_webppl',
+      version='0.1',
+      description='Jupyter magics',
+      url='http://github.com/tbenst/jupyter_webppl',
+      author='Tyler Benster',
+      author_email='code@tylerbenster.com',
+      license='GPLv3',
+      packages=['jupyter_webppl'],
+      zip_safe=False)
